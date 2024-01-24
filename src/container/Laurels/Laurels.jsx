33b57ed445fa4ebdce63ea8f800be8e5039ad0ef
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2024-01-24 11:24:07 +0700

    menambah fitur gallery hari ke tiga

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -8,7 +8,7 @@ const AwardCard = ({ award: {imgUrl, title, subtitle } }) => (
   <div className='app__laurels_awards-card'>
     <img src={imgUrl} alt="awards" />
     <div className='app__laurels_awards-card_content'>
-      <p className='p__cormorant' style={{ color: '#DCCA87'} }>{title}</p>
+      <p className='p__cormorant' style={{ color: '#DCCA87'}}>{title}</p>
       <p className='p__opensans'>{subtitle}</p>
     </div>
   </div>
