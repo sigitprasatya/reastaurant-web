a6f7a9310d91c243769cec0d8b86b5066c192e9e
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2024-01-24 14:43:10 +0700

    memperbaiki fitur footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './Newsletter.css';
+import SubHeading from '../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="NewsLetter" />
+      <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+      <p className='p__opensan'>And never miss latest Updates!</p>
+    </div>
+
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your email address' />
+      <button type='button' className='custom__button'>Subscribe</button>
+    </div>
   </div>
 );
 
